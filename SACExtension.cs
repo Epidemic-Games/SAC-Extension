233caf848b5dd261d398386b199c263cf606cd66
aldinioh <aldin0505@gmail.com>
2024-08-01 14:51:17 +1000

    Add extension details

--- SACExtension.cs
+++ SACExtension.cs
@@ -1,6 +1,20 @@
-﻿namespace Oxide.Ext.SAC
+﻿using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.SAC
 {
-    public class SACExtension
+    public class SACExtension : Extension
     {
+        public SACExtension(ExtensionManager manager) : base(manager) { }
+
+        // Initialise extenstion details
+        public override string Name => "Super Anti-Cheat";
+        public override string Author => "Epidemic";
+        public override VersionNumber Version => new(0, 0, 1);
+
+        // Called when the extension is loaded
+        public override void Load()
+        {
+        }
     }
 }
