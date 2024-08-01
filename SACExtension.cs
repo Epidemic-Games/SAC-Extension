3ab796c03c734a8cf09a607566656fff0643eb84
aldinioh <aldin0505@gmail.com>
2024-08-01 16:20:15 +1000

    Implement functionality for logging, also add ILog inheritance for Logger.

--- SACExtension.cs
+++ SACExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Oxide.Core;
 using Oxide.Core.Extensions;
+using Oxide.Ext.SAC.Logging;
 
 namespace Oxide.Ext.SAC
 {
@@ -12,9 +13,13 @@ namespace Oxide.Ext.SAC
         public override string Author => "Epidemic";
         public override VersionNumber Version => new(0, 0, 1);
 
+        // Creating a logger so that other parts of the extension can use it
+        public static ILog? ExtensionLogger;
+
         // Called when the extension is loaded
         public override void Load()
         {
+            ExtensionLogger = new Logger(LogLevel.Info);
         }
     }
 }
