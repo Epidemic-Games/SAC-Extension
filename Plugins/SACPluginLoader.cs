de7e6497262149bb12632d1f2a2dbfacb33f3c85
aldinioh <aldin0505@gmail.com>
2024-08-03 23:13:28 +1000

    Add sac command, as well as UICore.cs

--- Plugins/SACPluginLoader.cs
+++ Plugins/SACPluginLoader.cs
@@ -5,6 +5,6 @@ namespace Oxide.Ext.SAC.Plugins
 {
     public class SACPluginLoader : RustPluginLoader
     {
-        public override Type[] CorePlugins => new[] { typeof(SACExtensionCore) };
+        public override Type[] CorePlugins => new[] { typeof(SACExtensionCore), typeof(UICore) };
     }
 }
