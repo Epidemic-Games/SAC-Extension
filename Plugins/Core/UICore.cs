272720db2b46f90e1a36e9340966bbca0e58000a
aldinioh <aldin0505@gmail.com>
2024-08-04 11:22:05 +1000

    Add UI functions in UICore

--- Plugins/Core/UICore.cs
+++ Plugins/Core/UICore.cs
@@ -1,8 +1,54 @@
-﻿namespace Oxide.Ext.SAC.Plugins
+﻿using Oxide.Core.Plugins;
+
+namespace Oxide.Ext.SAC.Plugins
 {
     internal class UICore : SACPlugin
     {
+        public static UICore _instance;
+
         public UICore() 
+        {
+
+        }
+
+        [HookMethod(nameof(Init))]
+        private void Init()
+        {
+            _instance = this;
+        }
+
+        [HookMethod(nameof(DisplayOverviewUI))]
+        private void DisplayOverviewUI()
+        {
+
+        }
+
+        [HookMethod(nameof(DisplayPlayersUI))]
+        private void DisplayPlayersUI()
+        {
+
+        }
+
+        [HookMethod(nameof(DisplayPlayersUI))]
+        private void DisplayReportsUI()
+        {
+
+        }
+
+        [HookMethod(nameof(CreatePanel))]
+        private void CreatePanel()
+        {
+
+        }
+
+        [HookMethod(nameof(CreateButton))]
+        private void CreateButton()
+        {
+
+        }
+
+        [HookMethod(nameof(CreateIconButton))]
+        private void CreateIconButton()
         {
 
         }
